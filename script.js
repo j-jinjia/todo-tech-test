5d6194a03243627469e2938b11a93a97862f9239
j-jinjia <j.jinjia99@gmail.com>
2022-03-01 16:56:50 +0100

    Adding new tasks by input and add button working

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 //DOM Elements
-const inputField = document.querySelector(".inputBar__input").value;
+const inputField = document.querySelector(".inputBar__input");
 const inputFieldButton = document.querySelector(".inputBar__addBtn");
 const taskDisplay = document.querySelector(".tasks__list");
+const noTaskDisplay =document.querySelector(".tasks__empty")
 //Global Variables
 //Functions
-const inputFieldGetValue = (event) =>{
-    const inputValue = event.target.value
-}
 
 const onClickInputFieldButton = () =>{
-
-taskDisplay.innerHTML+=`<li><input type="checkbox">${inputField}</input><button>Edit</button><li>`
+    noTaskDisplay.innerText=""
+    taskDisplay.innerHTML+=`<li><input type="checkbox"> ${inputField.value} </input><button>Edit</button><li>`
+    inputField.value=""
 }
 //Event Listeners
 
 inputFieldButton.addEventListener("click", onClickInputFieldButton);
-inputField.addEventListener("input", inputFieldGetValue);
+inputField.addEventListener("",onClickInputFieldButton)
